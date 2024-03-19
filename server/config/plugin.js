47331964f009288415992d930717793514e75cd4
jon <username@user.com>
2024-03-19 23:37:37 +0800

    modified plugin.js

--- server/config/plugin.js
+++ server/config/plugin.js
@@ -2,9 +2,9 @@ module.exports = ({ env }) => ({
   "vercel-deploy": {
     enabled: true,
     config: {
-      deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
-      apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
-      appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
+      deployHook: env("VERCEL_DEPLOY_PLUGIN_HOOK"),
+      apiToken: env("VERCEL_DEPLOY_PLUGIN_API_TOKEN"),
+      appFilter: env("VERCEL_DEPLOY_PLUGIN_APP_FILTER"),
       //
       // teamFilter: "your-team-id-on-vercel",
       // roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
